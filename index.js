9a358288519a6faf427c1514ee9668204bca2dec
Scott Mai <scottmai702@gmail.com>
2022-06-25 23:00:38 -0700

    email

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const { initDatabase, getCollection } = require('./db');
 const cors = require('cors');
 const { ObjectId } = require('mongodb');
 const path = require('path');
+var nodemailer = require('nodemailer');
 
 const app = express();
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'maiscottie@gmail.com',
+        pass: process.env.DHIVS_EMAIL_APP_PASS,
+    }
+});
 
 app.get('/vouchers', async (req, res) => {
     const client = await initDatabase();
@@ -28,10 +37,24 @@ app.post('/redeem/:id', async (req, res) => {
 
     const client = await initDatabase();
     const collection = await getCollection(client);
-    const result = await collection.updateOne({ recipient: 'dhivs', _id: ObjectId(voucherId) }, { $set: { is_redeemed: true } })
-    console.log(
-        `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`,
-    );
+    const result = await collection.findOneAndUpdate({ recipient: 'dhivs', _id: ObjectId(voucherId) }, { $set: { is_redeemed: true } })
+    if (!result) {
+        console.log("no matching voucher found with id: " + voucherId)
+        return
+    }
+    const voucher = result.value
+    console.log("Found matching voucher: " + voucher.title)
+
+    const sendInfo = await transporter.sendMail({
+        from: 'dhivbot@gmail.com',
+        to: 'scottmai702@gmail.com',
+        subject: 'Dhivs has redeemed a voucher!',
+        text: `Dhivs redeemed voucher #${voucherId}: ${voucher.title}`
+    })
+
+    console.log({ sendInfo })
+
+
     client.close();
     res.json({})
 });
